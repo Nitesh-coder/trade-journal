0179a4e11de57693babd7131eea8c118e59be93e
Nitesh Ghimire <initesh100@gmail.com>
2024-08-30 14:49:10 +0545

    adjust mobile screen

--- frontend/src/Components/StatusBar.tsx
+++ frontend/src/Components/StatusBar.tsx
@@ -4,15 +4,15 @@ const StatusBar = () => {
     <div className="h-12 w-full bg-slate-900 text-slate-400 rounded-md sm:text-lg text-sm">
         <ul className=" flex justify-between items-center h-full px-1">
             <li>Status</li>
-            <li>Date</li>
+            <li className=" hidden sm:block">Date</li>
             <li>Pair</li>
             <li>Side</li>
             <li>Total Pips</li>
             <li>Lot Size</li>
             <li>Session</li>
             <li>%Changes</li>
-            <li>PNL</li>
-            <li>SetUp</li>
+            <li className=" hidden sm:block">PNL</li>
+            <li className=" hidden sm:block">SetUp</li>
         </ul>
     </div>
   )
