b79a2ab50d28035c2760363e7d73a49ba1b1775c
Nitesh Ghimire <initesh100@gmail.com>
2024-08-31 21:18:44 +0545

    fix

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,17 +1,17 @@
-import React, { useState } from "react"
+
 import InputBox from "../Components/InputBox"
 import Navbar from "../Components/Navbar"
 import RowData from "../Components/RowData"
 import StatusBar from "../Components/StatusBar"
 
 const Home: React.FC = () => {
-  interface Trade {
+/*   interface Trade {
     pair: string;
     side: string;
     pips: number;
     lot: number;
     session: string;
-  };
+  }; */
 /*   const [data, setData] = useState<Trade[]>([
     { pair: 'xauused', side: 'long', pips: 15, lot: 0.3, session: 'NY' },
     { pair: 'eurusd', side: 'short', pips: 10, lot: 0.2, session: 'LDN' },
