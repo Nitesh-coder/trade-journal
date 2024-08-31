3f9d59b417f9c5770b83bdbf4d7d0504260f76d0
Nitesh Ghimire <initesh100@gmail.com>
2024-08-31 21:27:29 +0545

    fix

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,19 +5,19 @@ import RowData from "../Components/RowData"
 import StatusBar from "../Components/StatusBar"
 
 const Home: React.FC = () => {
-/*   interface Trade {
+  interface Trade {
     pair: string;
     side: string;
     pips: number;
     lot: number;
     session: string;
-  }; */
+  };
 /*   const [data, setData] = useState<Trade[]>([
     { pair: 'xauused', side: 'long', pips: 15, lot: 0.3, session: 'NY' },
     { pair: 'eurusd', side: 'short', pips: 10, lot: 0.2, session: 'LDN' },
     { pair: 'usdjpy', side: 'long', pips: 20, lot: 0.5, session: 'TKY' },
   ]); */
-  const data = [
+  const data:Trade[] = [
     { pair: 'xauused', side: 'long', pips: 15, lot: 0.3, session: 'NY' },
     { pair: 'eurusd', side: 'short', pips: 10, lot: 0.2, session: 'LDN' },
     { pair: 'usdjpy', side: 'long', pips: 20, lot: 0.5, session: 'TKY' },
@@ -29,9 +29,10 @@ const Home: React.FC = () => {
           <div>Build Something Useful</div>
           <StatusBar />
           <div>
-            {data.map((index)=>(
+            {data.map((trade, index:number)=>(
               <div key={index}>
                 {<RowData />}
+                {console.log(trade.pair)}
               </div>
             ))}
           </div>
